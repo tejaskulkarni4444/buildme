2054c25cb921d472cd994b30560ff4c1aa03238f
tejas <tejas.kulkarni@n3hub.com>
2020-05-14 18:46:07 +0530

    Updates

---
+++ precache-manifest.b9852bdb83a0afaea603cbd5510c0160.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb7a6003fdb666b7d92d1ab91e8d68c2",
+    "url": "/buildme/index.html"
+  },
+  {
+    "revision": "1ff78433c3ac330967c8",
+    "url": "/buildme/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "997fe1ed7acc1b956bd2",
+    "url": "/buildme/static/js/2.e0ebc5d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/buildme/static/js/2.e0ebc5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff78433c3ac330967c8",
+    "url": "/buildme/static/js/main.0d95292c.chunk.js"
+  },
+  {
+    "revision": "8c1c17ac914edd8f43fe",
+    "url": "/buildme/static/js/runtime-main.195d657c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/buildme/static/media/logo.5d5d9eef.svg"
+  }
+]);
